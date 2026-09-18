601fed52e579b870d14ce9772e8fd3442834c972
0seg <chessemi00@gmail.com>
2026-09-17 22:36:42 -0600

    Add Destructors.cpp to implement a Cylinder class with destructor demonstration in C++

---
+++ Classe/Destructors.cpp
@@ -0,0 +1,55 @@
+/* Destructors in C++*/
+
+#include <iostream>
+
+class Cylinder {
+    private:
+        double radius{1};
+        double height{1};
+
+    public:
+        // Setter for radius
+        void setRadius(double r) {
+            radius = r;
+        }
+
+        // Getter for radius
+        double getRadius() {
+            return radius;
+        }
+
+        // Setter for height
+        void setHeight(double h) {
+            height = h;
+        }
+
+        // Getter for height
+        double getHeight() {
+            return height;
+        }
+
+        double volume() {
+            return 3.14159 * radius * radius * height;
+        }
+
+        // Destructor
+        ~Cylinder() {
+            std::cout << "Destructor called for Cylinder object." << std::endl;
+        }
+};
+
+
+int main(){
+    Cylinder c1; // Create an object of the Cylinder class
+
+    c1.setRadius(5.0); // Set the radius using the setter
+    c1.setHeight(10.0); // Set the height using the setter
+
+    std::cout << "Radius of the cylinder: " << c1.getRadius() << std::endl; // Get the radius using the getter
+    std::cout << "Height of the cylinder: " << c1.getHeight() << std::endl; // Get the height using the getter
+    std::cout << "Volume of the cylinder: " << c1.volume() << std::endl; // Get the volume using the method
+
+
+
+    return 0;
+}
